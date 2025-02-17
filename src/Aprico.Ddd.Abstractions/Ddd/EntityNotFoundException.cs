28da98adfb55511255bc19839cda83e76bddadd5
François Chabot <francois.chabot@stateless.be>
2025-02-17 17:23:14 +0100

    Improve EntityNotFoundException by adding helper methods
    Added two new static methods, `ThrowIfNull` and `Throw`, to the `EntityNotFoundException` class to assist in handling null entity checks and increasing error reporting efficiency. This update widens the class's range of use, offering more detailed error reports which include information about the entity type and key. The main goal of these enhancements is to provide user-friendly error reports that can assist in debugging by providing specific details including the type and key of the entity involved in the exception.

--- src/Aprico.Ddd.Abstractions/Ddd/EntityNotFoundException.cs
+++ src/Aprico.Ddd.Abstractions/Ddd/EntityNotFoundException.cs
@@ -1,13 +1,13 @@
 #region region Copyright & License
 
 // Copyright © 2024 - 2025 Aprico Consultants
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,6 +18,7 @@
 
 using System;
 using System.Diagnostics.CodeAnalysis;
+using Aprico.Ddd.Abstractions;
 
 namespace Aprico.Ddd;
 
@@ -26,16 +27,54 @@ namespace Aprico.Ddd;
 /// This exception is typically used in scenarios where an operation is attempted on an entity that does not exist in the
 /// underlying data store or context.
 /// </remarks>
-[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "Public API.")]
+[SuppressMessage("ReSharper", "MemberCanBeInternal", Justification = "Public API.")]
 public class EntityNotFoundException : Exception
 {
+	/// <summary>Throws an <see cref="EntityNotFoundException"/> if the specified entity is null.</summary>
+	/// <typeparam name="TEntity">The type of the entity.</typeparam>
+	/// <typeparam name="TKey">The type of the entity's key.</typeparam>
+	/// <param name="entity">The entity to check for null.</param>
+	/// <param name="id">The key associated with the entity.</param>
+	/// <exception cref="EntityNotFoundException">
+	/// Thrown when the <paramref name="entity"/> is null. If the <paramref name="id"/> is
+	/// provided, the exception message will include the Id.
+	/// </exception>
+	[SuppressMessage("ReSharper", "MemberCanBeInternal", Justification = "Public API.")]
+	public static void ThrowIfNull<TEntity, TKey>([NotNull] TEntity? entity, TKey? id = null)
+		where TEntity : Entity<TKey>
+		where TKey : struct
+	{
+		if (entity is null) Throw(entity, id);
+	}
+
+	/// <summary>Throws an <see cref="EntityNotFoundException"/> for the specified entity type.</summary>
+	/// <typeparam name="TEntity">The type of the entity.</typeparam>
+	/// <typeparam name="TKey">The type of the entity's key.</typeparam>
+	/// <param name="_">The entity being referenced.</param>
+	/// <param name="id">The key associated with the entity.</param>
+	/// <exception cref="EntityNotFoundException">
+	/// Always thrown to indicate that the entity of type <typeparamref name="TEntity"/>
+	/// could not be found. If the <paramref name="id"/> is provided, the exception message will include the Id.
+	/// </exception>
+	[SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "Public API.")]
+	public static void Throw<TEntity, TKey>([NotNull] TEntity? _, TKey? id = null)
+		where TEntity : Entity<TKey>
+		where TKey : struct
+	{
+		throw id.HasValue
+			? new EntityNotFoundException($"Entity '{nameof(TEntity)}' {{ {nameof(Entity<TKey>.Id)}: '{id.Value}' }} not found.")
+			: new EntityNotFoundException($"Entity '{nameof(TEntity)}' not found.");
+	}
+
 	/// <summary>Initializes a new instance of the <see cref="EntityNotFoundException"/> class with default values.</summary>
 	/// <remarks>Use this constructor when no specific message or inner exception needs to be provided.</remarks>
+	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Public API.")]
 	public EntityNotFoundException() { }
 
 	/// <summary>Initializes a new instance of the <see cref="EntityNotFoundException"/> class with a specified error message.</summary>
 	/// <param name="message">A message that describes the error.</param>
 	/// <remarks>Use this constructor to provide a specific error message that gives more context about the missing entity.</remarks>
+	[SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "Public API.")]
 	public EntityNotFoundException(string message) : base(message) { }
 
 	/// <summary>
@@ -48,5 +87,6 @@ public class EntityNotFoundException : Exception
 	/// exception is specified.
 	/// </param>
 	/// <remarks>Use this constructor to capture additional details about the underlying cause of the exception.</remarks>
+	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Public API.")]
 	public EntityNotFoundException(string message, Exception innerException) : base(message, innerException) { }
 }
