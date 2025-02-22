91c3d193f0c75f9d2c8d1c9b70030c9227cda993
François Chabot <francois.chabot@stateless.be>
2025-02-22 17:43:22 +0100

    Remove local EnumerableExtensions and switch to external library
    The custom `EnumerableExtensions` was removed, and usage replaced with the equivalent from `Be.Stateless.Extensions`. The necessary package reference and NuGet source configurations were added to support this change, reflecting a dependency shift to an external library.

--- src/Aprico.Ddd.Abstractions/Ddd/Abstractions/Entity.cs
+++ src/Aprico.Ddd.Abstractions/Ddd/Abstractions/Entity.cs
@@ -1,13 +1,13 @@
 #region Copyright & License
 
 // Copyright © 2024 - 2025 Aprico Consultants
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +23,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using Be.Stateless.Extensions;
 
 namespace Aprico.Ddd.Abstractions;
 
@@ -71,7 +72,7 @@ public abstract class Entity : IFormattable
 	{
 		internal static string EnsureNotNullOrEmptyOrValidate(string? format)
 		{
-			if (string.IsNullOrEmpty(format))
+			if (format.IsNullOrEmpty())
 				format = DEFAULT;
 			else
 				ThrowIfInvalid(format);
